5251d73311b2c5c72a2e5f9f00939e5e95dd93cf
ajith <ajithraveendranr@gmail.com>
2022-07-25 15:35:12 +0530

    with space

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import '../styles/App.css';
 import { UserProfile } from './UserProfile';
 
-
+const obj = {name:"Newton", age:3};
+const ThemeContext = React.createContext(obj);
 const App = () => {
+
+  const value = useContext(ThemeContext);
 
   return (
     <div id="main">
-      
-        <UserProfile />
+        <UserProfile value = {value} />
       
     </div>
   )
@@ -16,4 +18,4 @@ const App = () => {
 
 
 export default App;
-export {UserContext}
+// export {UserContext}
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,8 +2,13 @@ import { useContext } from 'react'
 import React from 'react'
 import { UserContext } from "./App"
 
-const UserProfile = () =>{
-
+const UserProfile = (props) =>{
+    
+  return(  <>
+<h1 id="name">Name:- {props.value.name}</h1>
+<h1 id="age">Age:- e{props.value.age}</h1>
+    </>
+  )
 }
 
 export {UserProfile}
